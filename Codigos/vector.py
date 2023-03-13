97e2795ea624ac13f89aaa531fd1b9c33434a4e1
Santiago Echeverri Arteaga <secheverri@uniquindio.edu.co>
2023-03-13 17:57:06 -0500

    Agrego modificacion a clase vector

--- Codigos/vector.py
+++ Codigos/vector.py
@@ -1,15 +1,41 @@
-class vector:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.dim = 2
-        self.norm = (x**2 + y**2)**0.5
+class matriz:
+    def __init__(self, valores):
+        self.valores = valores
+        n = len(valores)
+        m = len(valores[0])
+        self.shape = [n, m]
     def __str__(self):
-        respuesta = "(" + str(self.x) + ", " + str(self.y) +")"
+        if self.shape[0]==1:
+            respuesta = self._imprimir_vector_(0)
+            return(respuesta)
+        else:
+            respuesta = ""
+            for i in range(self.shape[0]-1):
+                respuesta = respuesta + self._imprimir_vector_(i)+"\n"
+            respuesta = respuesta + self._imprimir_vector_(self.shape[0]-1)
+            return(respuesta)
+    def _imprimir_vector_(self,fila):
+        respuesta = "|"
+        for i in range(self.shape[1]-1):
+            respuesta = respuesta + str(self.valores[fila][i])
+            respuesta = respuesta + " "
+            
+        respuesta = respuesta + str(self.valores[fila][self.shape[1]-1]) + "|"
         return(respuesta)
-    def __add__(self, other):
-        respuesta = vector(self.x+other.x, self.y+other.y)
-        return(respuesta)
+        
+    def __add__(self, otro):
+        if self.shape[0]==1:
+            lista = []
+            for i in range(self.shape[1]):
+                lista.append(self.valores[0][i]+otro.valores[0][i])
+            respuesta = matriz([lista])
+            return(respuesta)
+        else:
+            respuesta = self.valores
+            for i in range(self.shape[0]):
+                for j in range(self.shape[1]):
+                    respuesta[i][j] = self.valores[i][j]+otro.valores[i][j]
+            return(matriz(respuesta))
     def __mul__(self,otro):
         if (type(otro)==type(self)):
             return(3)
@@ -20,8 +46,7 @@ class vector:
             return(3)
         else:
             return(2)
-V1= vector(1.2,2.2)
-V2= vector(3.1,4.2)
-##print(V1+V2+V1+V2+V2)
-print(1000*V2)
-print(V2*V1)
+V1= matriz([[1,2,3, 5, 690, 0],[100,200,300, 500, 600, 0]])
+V2= matriz([[10,20,30, 0, 10, 30],[11,21,31, 0, 10, 30]])
+#print(type(V1+V2))
+print(V1+V2)
