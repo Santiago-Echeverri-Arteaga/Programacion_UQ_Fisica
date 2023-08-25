e9592cb06b8f521591f80e3f0fcd7154cec4862d
Santiago Echeverri Arteaga <secheverri@uniquindio.edu.co>
2023-08-25 17:49:21 -0500

    Agrego cambios clase viernes 25

--- Codigos/class vector.py
+++ Codigos/class vector.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 class vector:
     """Clase vector en 2D con coordenadas cartesianas y polares.
     Permite suma entre vectores y multiplicación"""
@@ -11,13 +14,28 @@ class vector:
     def __str__(self):
         cadena="["+str(self.x)+" "+str(self.y)+"]"
         return(cadena)
+    def __abs__(self):
+        return(self.magnitud)
+    def __call__(self, m:float):
+        """Metodo que recibe la masa m como flotante y retorna un vector con
+        correspondiente al vector ingresado dividido la masa
+
+        Args:
+            m (float): masa
+        """
+        return(vector(self.x/m, self.y/m))
 #print(type(str(21)))
 F1=vector(2,3)
-F2=vector(-2,3)
-print(F1)
-print(F2)
-Resultado=F1 + F2 +F1 + F2 +F2
-print(Resultado.magnitud)
+print(abs(F1))
+#F2=vector(-2,3)
+#print(F1(3))
+if F1(2).magnitud==0:
+    print("Está en equilibrio")
+else:
+    print("No esta en equilibrtio")
+#print(F2)
+#Resultado=F1 + F2 +F1 + F2 +F2
+#print(Resultado.magnitud)
 #name=input("Ingrese su nombre: ")
 #print("Hola",name)
 #edad=float(input("Ingrese su edad: "))
